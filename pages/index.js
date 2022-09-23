9b0af82a585e60a9994c34141722a13cbd88fc76
Majikujanisch <karottennase123@gmail.com>
2022-09-23 09:46:11 +0200

    +Dropdown component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,27 @@
 import Head from 'next/head'
 import style from '../styles/Login.module.css'
+import React, { useCallback } from 'react';
+import Dropdown from '../component/Dropdown';
+const items = [
+  {
+    label:"RAUM AUSWAHL",
+    value: "0"
+  },
+  {
+      label: "Raum 1",
+      value: "1"
+  },
+  {
+      label: "Raum 2",
+      value: "2"
+  },
+  {
+      label: "Raum 3",
+      value: "3"
+  },
+];
 export default function Login() {
+  const handleStateChange = useCallback((state) => console.log(state), []);
   return (
     <div className="container">
       <Head>
@@ -9,6 +30,16 @@ export default function Login() {
       </Head>
 
       <main>
+        <div className={style.Dropdown}>
+          <div className={style.Dropdown}>
+            <h2>Raumauswahl </h2>
+              <Dropdown
+                items={ items }
+                id="month-switcher"
+                onStateChange={ handleStateChange }
+                defaultSelectedItem={items[0]} />
+          </div>
+        </div>
         <div className={style.main}>
           <p>inputCode</p>
         </div>
