684f33f1dabfd788771a316a3dab9052262e724b
Majikujanisch <karottennase123@gmail.com>
2022-09-28 12:03:11 +0200

    Authentifikation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import React, { useCallback } from 'react';
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router"
 
 import Dropdown from '../component/Dropdown'
 import Numberpad from '../component/Numberpad';
@@ -26,10 +27,60 @@ const items = [
       value: "3"
   },
 ];
+//Session Check
+import { Configuration, V0alpha2Api, Session, Identity } from "@ory/client"
+import { edgeConfig } from "@ory/integrations/next"
+
+const ory = new V0alpha2Api(new Configuration(edgeConfig))
+
+
+// Returns either the email or the username depending on the user's Identity Schema
+const getUserName = (identity:Identity) => identity.traits.email || identity.traits.username
+
+
 export default function Login() {
+  //Session Handling
+  const router = useRouter()
+  const [session, setSession] = useState<Session | undefined>()
   //state for room to handle
   const [selectedRoom, setSelectedRoom] = useState(0);
   const handleStateChange = useCallback((onSelectedItemChange) => setSelectedRoom(onSelectedItemChange.selectedItem.value) ,[]);
+  useEffect(() => {
+    ory
+      .toSession()
+      .then(({ data }) => {
+        // User has a session!
+        setSession(data)
+      })
+      .catch(() => {
+        // Redirect to login page
+        return router.push(edgeConfig.basePath + "/ui/login")
+      })
+  })
+
+  if (!session) {
+    // Still loading
+    return null
+  }
+  const route = (req: Request, res: Response) => {
+    ory.createSelfServiceLogoutFlowUrlForBrowsers(session.id).then(({data}) => {
+        console.log(data.logout_url) // The logout URL
+        console.log(data.logout_token) // The logout token
+        console.log(session)
+        // You can render the logout URL like so:
+       
+      
+        // Or call the logout token:
+        ory.submitSelfServiceLogoutFlow(data.logout_token).then(() => {
+          
+         })
+         return <p>logout</p>
+      })
+  }
+
+
+
+ 
   //(onSelectedItemChange) => setSelectedRoom(onSelectedItemChange.selectedItem.value)
   //(onSelectedItemChange) => console.log(onSelectedItemChange.selectedItem.value)
   return (
@@ -40,7 +91,11 @@ export default function Login() {
       </Head>
 
       <main className={style.main}>
+            {
+              getUserName(session?.identity)
+            }
         <div className={style.Dropdown}>
+          <button onClick={route}>logout</button>
           <div className={style.Dropdown}>
             <h2>Raumauswahl </h2>
               <Dropdown
@@ -50,10 +105,6 @@ export default function Login() {
                 defaultSelectedItem={items[0]} 
                 />
           </div>
-        </div>
-        <div className={style.main}>
-          <Numberpad></Numberpad>
-          <APIButton></APIButton>
         </div>
       </main>
 
